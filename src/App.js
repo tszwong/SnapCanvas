718988877a126d10660c569505bda28c6d1c8e31
tszwong <wongt@bu.edu>
2023-11-08 23:24:05 -0500

    installed React Bootstrap package, updated the navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import {
     Link
 } from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
 import HomePage from './pages/HomePage';
 import AboutUsPage from './pages/AboutUsPage';
 import HistoryPage from './pages/HistoryPage';
@@ -16,25 +23,47 @@ function App() {
     return (
         <Router>
             <div>
-                {/* Navigation */}
-                <nav className="navbar bg-body-tertiary"> {/* Use className instead of class */}
-                    <div className="container-fluid"> {/* Use className instead of class */}
-                        <ul>
-                            {/*this will replaced with logo later*/}
-                            <li><Link to="/">Home</Link></li>
-                            <li>
-                                <div className="search-container">
-                                    <input type="text" placeholder="Search..." />
-                                    <button>Search</button>
-                                </div>
-                            </li>
-                            <li><Link to="/about-us">About Us</Link></li>
-                            <li><Link to="/history">History</Link></li>
-                            <li><Link to="/membership">Membership</Link></li>
-                            <li><Link to="">Upload</Link></li>
-                        </ul>
-                    </div>
-                </nav>
+                {/* Navigation, using React Bootstrap to make things easier */}
+                {/* when screen is a certain size it will turn into hamburger nav */}
+                <Navbar expand="lg" className="bg-body-tertiary">
+                    <Container fluid>
+                        {/* will be used when we have some sort of logo created */}
+                        {/*<Navbar.Brand as={Link} to="/">*/}
+                        {/*    <img src={logo} alt="Logo" width="30" height="30" />*/}
+                        {/*</Navbar.Brand>*/}
+
+                        <Navbar.Toggle aria-controls="navbar-nav" />
+                        <Navbar.Collapse id="navbar-nav">
+
+                            {/* home link, potentially replaced with logo later */}
+                            <Nav className="me-auto" style={{ marginLeft: "5vw" }}>
+                                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            </Nav>
+
+                            {/* search bar + button */}
+                            <div className="d-flex flex-grow-1">
+                                <Form className="d-flex w-100">
+                                    <FormControl
+                                        type="search"
+                                        placeholder="Search..."
+                                        className="me-2"
+                                        aria-label="Search"
+                                        style={{ marginLeft: "5vw", maxWidth: "50%" }}  // Adjust this percentage as needed
+                                    />
+                                    <Button variant="outline-primary">Search</Button>
+                                </Form>
+                            </div>
+
+                            {/* Right-aligned links */}
+                            <Nav style={{ marginRight: "5vw" }}>
+                                <Nav.Link as={Link} to="/about-us">About Us</Nav.Link>
+                                <Nav.Link as={Link} to="/history">History</Nav.Link>
+                                <Nav.Link as={Link} to="/membership">Membership</Nav.Link>
+                                <Nav.Link as={Link} to="/upload">Upload</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
 
 
                 {/* Routes */}
