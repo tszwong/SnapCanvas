829b5bb3149e74d424d19e309a00b04ba48483a9
tszwong <wongt@bu.edu>
2023-12-05 01:17:12 -0500

    added content for history page, replaced all placeholders, updated and added several components, css done for most parts, added more styles for darkmode, replaced all placeholder images for about us page

--- src/components/PhotoPost.js
+++ src/components/PhotoPost.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // one third image, two third text component
-const ImgDesc = ({ imageSrc, altText, id, title, date, description }) => (
-    <div className="row">
-        <div className="col-4">
-            <img src={imageSrc} alt={altText} className="img-fluid" id={id}/>
-        </div>
-        <div className="col-8">
-            <div>
-                <h3>{title} {date}</h3>
+const ImgDesc = ({ imageSrc, altText, id, title, date, description, readmore }) => {
+    const [isReadMoreVisible, setReadMoreVisible] = useState(false);
+
+    const toggleReadMore = () => {
+        setReadMoreVisible(!isReadMoreVisible);
+    };
+
+    return (
+        <div className="row">
+            <div className="col-4">
+                <img src={imageSrc} alt={altText} className="img-fluid" id={id}/>
             </div>
-            <div>{description}</div>
+            <div className="col-8">
+                <div>
+                    <h3><span id="photo-post-title">{title}</span> | <span id="photo-post-date">{date}</span></h3>
+                    <hr/>
+                </div>
+                <div id="photo-post-desc">
+                    {description}
+                    {isReadMoreVisible && <span className="read-more-text">{readmore}</span>}
+                </div>
+                <div id="photo-post-btn">
+                    <button id={"read-more-btn"} onClick={toggleReadMore}>
+                        {isReadMoreVisible ? 'Read Less' : 'Read More'}
+                    </button>
+                </div>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default ImgDesc;
